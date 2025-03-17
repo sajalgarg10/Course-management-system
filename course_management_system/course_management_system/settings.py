9a7a756c0f5a1760bfdb04b1103e26b926fda6ad
sajalgarg10 <sajal.garg10@gmail.com>
2025-03-17 19:17:36 +0530

    postgres

--- course_management_system/course_management_system/settings.py
+++ course_management_system/course_management_system/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'course_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'course-management-system',
+        'USER': 'postgres',
+        'PASSWORD': 'sajal',
+        'HOST': 'localhost'
     }
 }
 
